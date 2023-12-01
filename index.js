5976e1420c3bda3339d0f7362421a620852646ec
Tobejizzle <71057635+Tobejizzle@users.noreply.github.com>
2023-11-30 18:10:59 -0800

    Update index.js

--- index.js
+++ index.js
@@ -12,3 +12,46 @@ function isElementInViewport (el) {
 function scrollView(elemID) {
     document.getElementById(elemID).scrollIntoView({behavior: "smooth", block: "center"});
 }
+
+// Function to set a cookie
+function setCookie(name, value, days) {
+    var expires = "";
+    if (days) {
+        var date = new Date();
+        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+        expires = "; expires=" + date.toUTCString();
+    }
+    document.cookie = name + "=" + (value || "") + expires + "; path=/";
+}
+
+// Function to get a cookie
+function getCookie(name) {
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(';');
+    for (var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') c = c.substring(1, c.length);
+        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
+    }
+    return null;
+}
+
+// Function to track page view
+function trackPageView() {
+    var pagePath = window.location.pathname;
+    var visitTimestamp = new Date().toUTCString();
+
+    // Get or create a unique visitor ID
+    var visitorId = getCookie('visitorId');
+    if (!visitorId) {
+        visitorId = Math.random().toString(36).substring(2) + Date.now().toString(36);
+        setCookie('visitorId', visitorId, 365); // Set a cookie that expires in 10 days
+    }
+
+    console.log('Visitor ID:', visitorId);
+    console.log('Page Path:', pagePath);
+    console.log('Visit Timestamp:', visitTimestamp);
+}
+
+// Track page view on page load
+window.addEventListener('load', trackPageView);
